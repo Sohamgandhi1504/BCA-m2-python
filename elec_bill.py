c0ee40ee91e6c1dab0a8d00b8688071fd753108a
Sohamgandhi1504 <soham.gandhi.1504@gmail.com>
2025-07-12 13:29:44 +0530

    Add files via upload

---
+++ elec_bill.py
@@ -0,0 +1,5 @@
+print("Welcome to Electricity Bill Generator !!")
+name = input("Your Name :")
+units = int(input("Enter Your Units :"))
+Bill = units * 6.7
+print(f"Greetings {name}\nYour Electricity Bill is : {Bill}")
